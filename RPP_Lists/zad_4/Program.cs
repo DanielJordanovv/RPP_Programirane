7a41bcbc29bc3602572fec12d5cfcb81958d6ff1
DanielJordanovv <d.jordanov2019@pmg-pernik.com>
2023-02-15 09:40:23 +0200

    Upload RPP_Lists

---
+++ RPP_Lists/zad_4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace zad_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = Console.ReadLine().Split().Select(int.Parse).ToList();
+            var result = new List<int>();
+            for (int i = 0; i < nums.Count; i++)
+            {
+                if (nums[i]==Math.Pow(Math.Sqrt(nums[i]),2))
+                {
+                    result.Add(nums[i]);
+                }
+            }
+            var output = result.OrderByDescending(x => x).ToList();
+            Console.WriteLine(string.Join(" ",output));
+        }
+    }
+}
